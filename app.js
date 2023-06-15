95aa8b65ffd4e150f378ecef63ece6ba9df4464a
Ilya Pyatnichuk <zein3288@gmail.com>
2023-06-16 01:21:16 +0300

    swagger

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const authRouter = require('./routes/api/auth');
 
 const app = express();
@@ -15,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
